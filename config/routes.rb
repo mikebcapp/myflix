7039b811b7f42f9387a4a68a7a980bc6ab7874a8
Mike <mikebcapp@gmail.com>
2016-01-28 11:45:21 +1300

    Changes to fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  get "/home" => "videos#index"
-
- 
+  get "/home", to: "videos#index"
+  
+  resources :videos, only: :show
+  resources :categories, only: :show
   
 end
