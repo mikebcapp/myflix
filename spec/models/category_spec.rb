5b3b5c2cde878ee9a38806fe6fe32c5f98a313a9
Mike <mikebcapp@gmail.com>
2016-02-04 06:13:56 +1300

    Re-push assignment to code review

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
 describe Category do 
-  it 'saves itself' do
-    category = Category.new(name: "Comedy")
-    category.save
-    expect(Category.first.name).to eq("Comedy")
-  end
-
-  it 'has many videos' do
-    comedies = Category.create(name: "comedies")
-    south_park = Video.create(title: "South Park", description: "Funny adult animated show", category: comedies)
-    futurama = Video.create(title: "Futurama", description: "Funny space animated show", category: comedies)
-    expect(comedies.videos).to include(futurama, south_park)
-  end
-
+  it { should have_many (:videos)}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,18 +1,8 @@
 require 'spec_helper'
 
 describe Video do 
-  it { belong_to (:category)}
+  it { should belong_to (:category)}
+  it { should validate_presence_of (:title)}
+  it { should validate_presence_of (:description)}
     
-
-  it "does not save a video without a title" do
-    video = Video.create(description: "A great video")
-    expect(Video.count).to eq(0)
-  end
-
-  it "does not save a video without a description" do
-    video = Video.create(title: "Southpark")
-    expect(Video.count).to eq(0)
-  end
-
-  
 end
