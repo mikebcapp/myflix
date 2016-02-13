6c937b8b32f4af19bacf1b50dabf8a8b1c1971b9
Mike <mikebcapp@gmail.com>
2016-02-14 12:01:20 +1300

    Redo in Category Model as set up in Video model by mistake

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :videos
+
+
+  def recent_videos
+    @recent = Video.take(6)
+    @recent.sort_by{ |a| a.created_at }.reverse
+  end
+
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,3 +3,31 @@ require 'spec_helper'
 describe Category do 
   it { should have_many (:videos) }
 end
+
+describe "#recent_videos" do
+  it "returns 6 videos when there are more than 6 videos in total" do
+    comedies = Category.create(name: "comedies")
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
+    southpark = Video.create(title: "SouthPark", description: "Crazy animated show", category: comedies)
+    back_to_future = Video.create(title: "Back to Future", description: "Time Travel", category: comedies)
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
+    southpark = Video.create(title: "SouthPark", description: "Crazy animated show", category: comedies)
+    back_to_future = Video.create(title: "Back to Future", description: "Time Travel", category: comedies)
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
+    expect(Video.recent_videos.count).to eq(6)
+  end
+  it "returns the total number of videos  when there are less than 6 total videos" do
+    comedies = Category.create(name: "comedies") 
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies)
+    southpark = Video.create(title: "SouthPark", description: "Crazy animated show", category: comedies)
+    back_to_future = Video.create(title: "Back to Future", description: "Time Travel", category: comedies)
+    expect(Video.recent_videos.count).to eq(3)
+  end
+  it "returns the videos in reverse chronological order ie, the most recent first" do
+    comedies = Category.create(name: "comedies")
+    futurama = Video.create(title: "Futurama", description: "Space Travel", category: comedies, created_at: 1.day.ago)
+    southpark = Video.create(title: "SouthPark", description: "Crazy animated show", category: comedies, created_at: 5.day.ago)
+    back_to_future = Video.create(title: "Back to Future", description: "Time Travel", category: comedies, created_at: 4.day.ago)
+    expect(Video.recent_videos).to eq([futurama, back_to_future, southpark])
+  end
+end
