70912c19fc7ab9f8b5489312d9b1cf1193592a40
Mike <mikebcapp@gmail.com>
2016-02-22 23:07:16 +1300

    Complete authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
+ 
   def new
+    redirect_to home_path if current_user
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       flash[:success] = "You have successfully logged in."
+       redirect_to home_path
+    else
+        flash[:danger] = "There is a problem with your username or password."
+        redirect_to sign_in_path
+     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:info] = "You have now signed out"
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
+  get :home, to: 'videos#index'
   get :register, to: 'users#new'
-  get :sign_in, to: 'users#sign_in'
+  get :sign_in, to: 'sessions#new'
+  get :sign_out, to: 'sessions#destroy'
   
   root to: "pages#front"
   
@@ -13,4 +15,5 @@ Myflix::Application.routes.draw do
 
   resources :categories, only: :show
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
